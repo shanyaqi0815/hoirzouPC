54aaaac27d6b3746fea935bf85b20c26fd4217d3
bob <1553859055@qq.com>
2020-03-13 15:44:14 +0800

    vue项目架构

---
+++ src/pages/index/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="hello1">
+      <el-row>
+        <el-col :span="24">
+          <h1>首页</h1>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <!-- 公共组件 -->
+      <bob></bob>
+      <!-- 局部组件 -->
+      <indexcom></indexcom>
+      <el-button @click="fn">123123213</el-button>
+  </div>
+</template>
+
+<script>
+import indexcom from "../index/components/indexcom"
+import bob from "../../Common/bob"
+import {add,fn} from '../../Axios/Http'
+export default {
+  name: 'hello1',
+  components:{
+      indexcom ,bob
+  },
+  data () {
+    return {
+      // loading:false,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created(){
+    add().then(res=>{
+      console.log(res)
+    })
+              var p ={
+            page:1
+          }
+          setTimeout(()=>{
+            fn(p).then(res=>{
+              console.log(res)
+            })
+          },3000)
+  },
+  methods:{
+    fn(){
+
+
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  lang="less" scoped>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
